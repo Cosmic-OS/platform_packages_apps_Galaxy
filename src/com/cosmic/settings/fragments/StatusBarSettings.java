2703340e5adbb4c3295a4bed9ab0ec7211d97d2a
Jmz <jmztaylor@gmail.com>
2015-10-29 22:24:07 +0000

    Force Expanded Notifications [2/2]
    Change-Id: Ic4a19d7cf0e0e451f5a8419dbc7044118df3de42

--- src/com/cosmic/settings/fragments/StatusBarSettings.java
+++ src/com/cosmic/settings/fragments/StatusBarSettings.java
@@ -16,25 +16,60 @@
 
 package com.cosmic.settings.fragments;
 
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.PreferenceCategory;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
-
-import com.android.internal.logging.MetricsProto.MetricsEvent;
-
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+ 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.settings.Utils;
 
-public class StatusBarSettings extends SettingsPreferenceFragment {
+public class StatusBarSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
+
+    private SwitchPreference mForceExpanded;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.statusbar_settings);
         final PreferenceScreen prefScreen = getPreferenceScreen();
+
+     mForceExpanded = (SwitchPreference) findPreference(FORCE_EXPANDED_NOTIFICATIONS);
+        mForceExpanded.setOnPreferenceChangeListener(this);
+        int ForceExpanded = Settings.System.getInt(getContentResolver(),
+                FORCE_EXPANDED_NOTIFICATIONS, 0);
+        mForceExpanded.setChecked(ForceExpanded != 0);
     }
 
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.GALAXY;
     }
+
+    @Override
+     public void onResume() {
+         super.onResume();
+     }
+
+    @Override
+     public boolean onPreferenceChange(Preference preference, Object newValue) {
+       if (preference == mForceExpanded) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(), FORCE_EXPANDED_NOTIFICATIONS,
+                    value ? 1 : 0);
+            return true;
+          }
+          return false;
+     }
 }
