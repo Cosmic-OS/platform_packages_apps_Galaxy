a4110302c11b38bca9498235dd8baab7a95b4ae3
bigrushdog <randall.rushing@gmail.com>
2016-09-21 03:48:11 -0700

    DUI: Initial N checkin
    Change-Id: I98e639868eeb4c5d5496f73cd6c67345c7c2e57b
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- src/com/cosmic/settings/fragments/NotificationDrawerSettings.java
+++ src/com/cosmic/settings/fragments/NotificationDrawerSettings.java
@@ -117,4 +117,3 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment imple
         }
     }
 }
-
--- src/com/cosmic/settings/fragments/VolumeRockerSettings.java
+++ src/com/cosmic/settings/fragments/VolumeRockerSettings.java
@@ -79,4 +79,3 @@ public class VolumeRockerSettings extends SettingsPreferenceFragment implements
         return false;
     }
 }
-
