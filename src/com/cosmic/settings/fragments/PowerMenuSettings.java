897aa22131077fcf0f8e1853392962044c12b9c1
LorDClockaN <davor@losinj.com>
2016-09-28 23:35:43 +0200

    Power/reboot dialogs dim [2/2]
    - Controls the dim amount of the power/reboot menu dialog
    
    Change-Id: Ia73f57730a139f1aabfc23f42379090317012d88

--- src/com/cosmic/settings/fragments/PowerMenuSettings.java
+++ src/com/cosmic/settings/fragments/PowerMenuSettings.java
@@ -17,6 +17,8 @@
 package com.cosmic.settings.fragments;
 
 import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.content.Intent;
 import android.content.pm.UserInfo;
 import android.os.Bundle;
@@ -25,6 +27,7 @@ import android.os.UserManager;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 
@@ -35,6 +38,7 @@ import com.android.settings.SettingsPreferenceFragment;
 
 import com.cosmic.settings.utils.Utils;
 import com.cosmic.settings.preferences.SystemSettingSwitchPreference;
+import com.cosmic.settings.preferences.CustomSeekBarPreference;
 
 import com.android.internal.util.cosmic.PowerMenuConstants;
 import static com.android.internal.util.cosmic.PowerMenuConstants.*;
@@ -45,10 +49,14 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PowerMenuSettings extends SettingsPreferenceFragment {
+public class PowerMenuSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String ACTION_CATEGORY = "action_category";
     private static final String POWER_CATEGORY = "power_category";
+    private static final String POWER_REBOOT_DIALOG_DIM = "power_reboot_dialog_dim";
+
+    private CustomSeekBarPreference mPowerRebootDialogDim;
+
     // power items
     private SwitchPreference mRebootPref;
     private SwitchPreference mScreenshotPref;
@@ -87,6 +95,12 @@ public class PowerMenuSettings extends SettingsPreferenceFragment {
         if (!lockPatternUtils.isSecure(MY_USER_ID)) {
             prefScreen.removePreference(powerCategory);
         }
+
+        mPowerRebootDialogDim = (CustomSeekBarPreference) prefScreen.findPreference(POWER_REBOOT_DIALOG_DIM);
+        int powerRebootDialogDim = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.POWER_REBOOT_DIALOG_DIM, 50);
+        mPowerRebootDialogDim.setValue(powerRebootDialogDim / 1);
+        mPowerRebootDialogDim.setOnPreferenceChangeListener(this);
 
 		// power items
         mAvailableActions = getActivity().getResources().getStringArray(
@@ -212,6 +226,17 @@ public class PowerMenuSettings extends SettingsPreferenceFragment {
     public void onResume() {
         super.onResume();
         updatePreferences();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mPowerRebootDialogDim) {
+            int alpha = (Integer) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWER_REBOOT_DIALOG_DIM, alpha * 1);
+            return true;
+        }
+        return false;
     }
 
     @Override
