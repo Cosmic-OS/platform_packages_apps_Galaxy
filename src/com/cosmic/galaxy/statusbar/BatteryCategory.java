4e36c07eaac6fa6527dd8206c3f2d1479a75c71f
maxwen <max.weninger@gmail.com>
2016-10-05 22:37:56 +0200

    [2/2] Battery styles are back
    Change-Id: Ia95a95bbec2240b96e675a19a2a9e408b99f3a4a
    OG: battery style: disable "Show percent" if "Text only"
    Author:ezio84

--- src/com/cosmic/galaxy/statusbar/BatteryCategory.java
+++ src/com/cosmic/galaxy/statusbar/BatteryCategory.java
@@ -16,23 +16,52 @@
 
 package com.cosmic.galaxy.statusbar;
 
+import android.app.AlertDialog;
 import android.app.ActivityManager;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.provider.SearchIndexableResource;
 import android.provider.Settings;
-import android.preference.Preference;
-import android.preference.PreferenceScreen;
-import android.preference.PreferenceCategory;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.SwitchPreference;
+import android.provider.Settings.SettingNotFoundException;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+
+import java.util.List;
+import java.util.ArrayList;
+import com.cosmic.galaxy.preference.SystemSettingSwitchPreference;
 
 public class BatteryCategory extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+    private static final String TAG = "BatteryCategory";
+
+    private static final String STATUSBAR_BATTERY_STYLE = "statusbar_battery_style";
+    private static final String STATUSBAR_BATTERY_PERCENT = "statusbar_battery_percent";
+    private static final String STATUSBAR_BATTERY_PERCENT_INSIDE = "statusbar_battery_percent_inside";
+    private static final String STATUSBAR_BATTERY_SHOW_BOLT = "statusbar_battery_charging_image";
+    private static final String STATUSBAR_CATEGORY_CHARGING = "statusbar_category_charging";
+
+    private ListPreference mBatteryStyle;
+    private ListPreference mBatteryPercent;
+    private SwitchPreference mPercentInside;
+    private SwitchPreference mShowBolt;
+    private int mShowPercent;
+    private int mBatteryStyleValue;
+    private int mShowBattery = 1;
 
     @Override
     protected int getMetricsCategory() {
@@ -45,6 +74,35 @@ public class BatteryCategory extends SettingsPreferenceFragment implements
 
         addPreferencesFromResource(R.xml.cosmic_battery);
 
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mBatteryStyle = (ListPreference) findPreference(STATUSBAR_BATTERY_STYLE);
+        mBatteryStyleValue = Settings.System.getInt(resolver,
+                Settings.System.STATUSBAR_BATTERY_STYLE, 0);
+        mBatteryStyle.setValue(Integer.toString(mBatteryStyleValue));
+        mBatteryStyle.setSummary(mBatteryStyle.getEntry());
+        mBatteryStyle.setOnPreferenceChangeListener(this);
+
+        mBatteryPercent = (ListPreference) findPreference(STATUSBAR_BATTERY_PERCENT);
+        mShowPercent = Settings.System.getInt(resolver,
+                Settings.System.STATUSBAR_BATTERY_PERCENT, 2);
+        mBatteryPercent.setValue(Integer.toString(mShowPercent));
+        mBatteryPercent.setSummary(mBatteryPercent.getEntry());
+        mBatteryPercent.setOnPreferenceChangeListener(this);
+
+        mPercentInside = (SwitchPreference) findPreference(STATUSBAR_BATTERY_PERCENT_INSIDE);
+
+        mShowBolt = (SwitchPreference) findPreference(STATUSBAR_BATTERY_SHOW_BOLT);
+
+        updateEnablement();
+
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        // If we didn't handle it, let preferences handle it.
+        return super.onPreferenceTreeClick(preference);
     }
 
     @Override
@@ -52,8 +110,36 @@ public class BatteryCategory extends SettingsPreferenceFragment implements
         super.onResume();
     }
 
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mBatteryStyle) {
+            mBatteryStyleValue = Integer.valueOf((String) newValue);
+            int index = mBatteryStyle.findIndexOfValue((String) newValue);
+            mBatteryStyle.setSummary(
+                    mBatteryStyle.getEntries()[index]);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.STATUSBAR_BATTERY_STYLE, mBatteryStyleValue);
+            updateEnablement();
+            return true;
+        } else if (preference == mBatteryPercent) {
+            mShowPercent = Integer.valueOf((String) newValue);
+            int index = mBatteryPercent.findIndexOfValue((String) newValue);
+            mBatteryPercent.setSummary(
+                    mBatteryPercent.getEntries()[index]);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.STATUSBAR_BATTERY_PERCENT, mShowPercent);
+            updateEnablement();
+            return true;
+        }
         return false;
+    }
+
+    private void updateEnablement() {
+        mBatteryPercent.setEnabled(mBatteryStyleValue != 5 && mBatteryStyleValue != 3);
+        mPercentInside.setEnabled(mBatteryStyleValue != 5 && mBatteryStyleValue < 3
+                && mShowPercent != 0);
+        mShowBolt.setEnabled(mBatteryStyleValue < 3);
     }
 }
 
