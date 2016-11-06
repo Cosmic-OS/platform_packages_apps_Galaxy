1cb1bf81b21445b0ff231a14c6f67e173dca0d38
Alex Cruz <mazdarider23@gmail.com>
2016-11-06 01:44:30 +0100

    Screenshot delay [2/2]
    Change-Id: I1cdf311e0472cb6a4e39d789dca7b65680915cd6
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- src/com/cosmic/settings/fragments/MiscSettings.java
+++ src/com/cosmic/settings/fragments/MiscSettings.java
@@ -16,21 +16,56 @@
 
 package com.cosmic.settings.fragments;
 
+import android.app.Activity;
+import android.app.ActivityManagerNative;
+import android.app.IActivityManager;
+import android.content.ContentResolver;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class MiscSettings extends SettingsPreferenceFragment {
+import com.cosmic.settings.preferences.CustomSeekBarPreference;
+
+public class MiscSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String SCREENSHOT_DELAY = "screenshot_delay";
+
+    private CustomSeekBarPreference mScreenshotDelay;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.misc_settings);
+
+        final ContentResolver resolver = getActivity().getContentResolver();
         PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mScreenshotDelay = (CustomSeekBarPreference) findPreference(SCREENSHOT_DELAY);
+        int screenshotDelay = Settings.System.getInt(resolver,
+                Settings.System.SCREENSHOT_DELAY, 1000);
+        mScreenshotDelay.setValue(screenshotDelay / 1);
+        mScreenshotDelay.setOnPreferenceChangeListener(this);
+   }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mScreenshotDelay) {
+            int screenshotDelay = (Integer) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SCREENSHOT_DELAY, screenshotDelay * 1);
+            return true;
+        }
+        return false;
     }
 
     @Override
