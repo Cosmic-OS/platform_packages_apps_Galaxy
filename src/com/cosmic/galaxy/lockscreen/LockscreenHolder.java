kronic-staging/platform_packages_apps_OwlsNest

Add Various Options menu to Lockscreen category
Change-Id: I0aa603e5864698e099402d0f3d1b4bc04e0386b6

src/com/aosip/owlsnest/lockscreen/LockscreenHolder.java
@@ -51,7 +51,7 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
-import com.aosip.owlsnest.lockscreen.WeatherCategory;
+import com.aosip.owlsnest.lockscreen.OptionsCategory;
 import com.aosip.owlsnest.PagerSlidingTabStrip;
 
 import java.util.ArrayList;
@@ -106,7 +106,7 @@ public class LockscreenHolder extends SettingsPreferenceFragment {
 
         public StatusBarAdapter(FragmentManager fm) {
             super(fm);
-            frags[0] = new WeatherCategory();
+            frags[0] = new OptionsCategory();
         }
 
         @Override
@@ -128,7 +128,7 @@ public class LockscreenHolder extends SettingsPreferenceFragment {
     private String[] getTitles() {
         String titleString[];
         titleString = new String[]{
-                    getString(R.string.weather_category)};
+                    getString(R.string.options_category)};
         return titleString;
     }
 }

src/com/aosip/owlsnest/lockscreen/OptionsCategory.java
@@ -1,0 +1,59 @@
+/*
+ * Copyright (C) 2016 Android Open Source Illusion Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aosip.owlsnest.lockscreen;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class OptionsCategory extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.OWLSNEST;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.aosip_options);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+}
+

src/com/aosip/owlsnest/recent/OmniCategory.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.aosip.recent;
+package com.aosip.owlsnest.recent;
 
 import android.app.ActivityManager;
 import android.content.Context;

Cosmic-OS/platform_packages_apps_Galaxy

src/com/cosmic/galaxy/lockscreen/LockscreenHolder.java
@@ -51,7 +51,7 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
-import com.cosmic.galaxy.lockscreen.WeatherCategory;
+import com.cosmic.galaxy.lockscreen.OptionsCategory;
 import com.cosmic.galaxy.PagerSlidingTabStrip;
 
 import java.util.ArrayList;
@@ -106,7 +106,7 @@ public class LockscreenHolder extends SettingsPreferenceFragment {
 
         public StatusBarAdapter(FragmentManager fm) {
             super(fm);
-            frags[0] = new WeatherCategory();
+            frags[0] = new OptionsCategory();
         }
 
         @Override
@@ -128,7 +128,7 @@ public class LockscreenHolder extends SettingsPreferenceFragment {
     private String[] getTitles() {
         String titleString[];
         titleString = new String[]{
-                    getString(R.string.weather_category)};
+                    getString(R.string.options_category)};
         return titleString;
     }
 }

src/com/cosmic/galaxy/lockscreen/OptionsCategory.java
@@ -1,0 +1,59 @@
+/*
+ * Copyright (C) 2016 Android Open Source Illusion Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cosmic.galaxy.lockscreen;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class OptionsCategory extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.GALAXY;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.cosmic_options);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+}
+

src/com/cosmic/galaxy/recent/OmniCategory.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.cosmic.recent;
+package com.cosmic.galaxy.recent;
 
 import android.app.ActivityManager;
 import android.content.Context;