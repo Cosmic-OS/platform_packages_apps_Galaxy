kronic-staging/platform_packages_apps_OwlsNest

Status Bar Logo 1/2
See base commit for details http://review.aosip.xyz/#/c/816/

Change-Id: I037ab4ae100efe842fdd370cc878f287d6b4d768

src/com/aosip/owlsnest/statusbar/StatusbarHolder.java
@@ -56,6 +56,7 @@ import com.aosip.owlsnest.statusbar.BatteryCategory;
 import com.aosip.owlsnest.statusbar.IconsCategory;
 import com.aosip.owlsnest.statusbar.ExtrasCategory;
 import com.aosip.owlsnest.statusbar.StatusBarTickerSettings;
+import com.aosip.owlsnest.statusbar.StatusBarLogo;
 import com.aosip.owlsnest.statusbar.TrafficCategory;
 import com.aosip.owlsnest.PagerSlidingTabStrip;
 
@@ -111,12 +112,13 @@ public class StatusbarHolder extends SettingsPreferenceFragment {
 
         public StatusBarAdapter(FragmentManager fm) {
             super(fm);
-            frags[0] = new BatteryCategory();
-            frags[1] = new ClockCategory();
-            frags[2] = new ExtrasCategory();
-            frags[3] = new IconsCategory();
-            frags[4] = new StatusBarTickerSettings();
-            frags[5] = new TrafficCategory();
+            frags[0] = new StatusBarLogo();
+            frags[1] = new BatteryCategory();
+            frags[2] = new ClockCategory();
+            frags[3] = new ExtrasCategory();
+            frags[4] = new IconsCategory();
+            frags[5] = new StatusBarTickerSettings();
+            frags[6] = new TrafficCategory();
         }
 
         @Override
@@ -138,7 +140,7 @@ public class StatusbarHolder extends SettingsPreferenceFragment {
     private String[] getTitles() {
         String titleString[];
         titleString = new String[]{
-
+                    getString(R.string.logo_category),
                     getString(R.string.battery_category),
                     getString(R.string.clock_category),
                     getString(R.string.extra_category),

Cosmic-OS/platform_packages_apps_Galaxy

src/com/cosmic/galaxy/statusbar/StatusbarHolder.java
@@ -56,6 +56,7 @@ import com.cosmic.galaxy.statusbar.BatteryCategory;
 import com.cosmic.galaxy.statusbar.IconsCategory;
 import com.cosmic.galaxy.statusbar.ExtrasCategory;
 import com.cosmic.galaxy.statusbar.StatusBarTickerSettings;
+import com.cosmic.galaxy.statusbar.StatusBarLogo;
 import com.cosmic.galaxy.statusbar.TrafficCategory;
 import com.cosmic.galaxy.PagerSlidingTabStrip;
 
@@ -111,12 +112,13 @@ public class StatusbarHolder extends SettingsPreferenceFragment {
 
         public StatusBarAdapter(FragmentManager fm) {
             super(fm);
-            frags[0] = new BatteryCategory();
-            frags[1] = new ClockCategory();
-            frags[2] = new ExtrasCategory();
-            frags[3] = new IconsCategory();
-            frags[4] = new StatusBarTickerSettings();
-            frags[5] = new TrafficCategory();
+            frags[0] = new StatusBarLogo();
+            frags[1] = new BatteryCategory();
+            frags[2] = new ClockCategory();
+            frags[3] = new ExtrasCategory();
+            frags[4] = new IconsCategory();
+            frags[5] = new StatusBarTickerSettings();
+            frags[6] = new TrafficCategory();
         }
 
         @Override
@@ -138,7 +140,7 @@ public class StatusbarHolder extends SettingsPreferenceFragment {
     private String[] getTitles() {
         String titleString[];
         titleString = new String[]{
-
+                    getString(R.string.logo_category),
                     getString(R.string.battery_category),
                     getString(R.string.clock_category),
                     getString(R.string.extra_category),