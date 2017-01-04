bd5c006ad672ef28ad838735263d3e6550d1c583
Sagar Shah <sagarshah2326@gmail.com>
2017-01-04 16:09:42 +0100

    Fix Building

--- src/com/cosmic/settings/fragments/HWKeysSettings.java
+++ src/com/cosmic/settings/fragments/HWKeysSettings.java
@@ -19,10 +19,12 @@ package com.cosmic.settings.fragments;
 import android.content.ContentResolver;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 
 import com.android.internal.logging.MetricsLogger;
