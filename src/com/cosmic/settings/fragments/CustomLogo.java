shivatejapeddi/packages_apps_CustomSquash

Custom logos ,Bring it Back[2/2]

src/com/citrus/settings/fragments/CustomLogo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The Dirty Unicorns project
+ * Copyright (C) 2016 Resurrection Remix
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,86 +23,82 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceScreen;
-import android.support.v7.preference.PreferenceCategory;
-import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceScreen;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 
 import com.android.settings.R;
-import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
-import com.android.internal.logging.MetricsProto.MetricsEvent;
-
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
-public class LogoSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+public class CustomLogo extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
 
-    public static final String TAG = "LogoSettings";
+    public static final String TAG = "Logo";
 
-    private static final String KEY_CITRUS_LOGO_COLOR = "status_bar_citrus_logo_color";
-    private static final String KEY_CITRUS_LOGO_STYLE = "status_bar_citrus_logo_style";
+    private static final String KEY_CUSTOM_LOGO_COLOR = "custom_logo_color";
+    private static final String KEY_CUSTOM_LOGO_STYLE = "custom_logo_style";
 
-    private ColorPickerPreference mCitrusLogoColor;
-    private ListPreference mCitrusLogoStyle;
+    private ColorPickerPreference mCustomLogoColor;
+    private ListPreference mCustomLogoStyle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.citrus_logo);
+        addPreferencesFromResource(R.xml.custom_logo);
 
         PreferenceScreen prefSet = getPreferenceScreen();
 
-        int intColor;
-        String hexColor;
- 
-        	// Citrus logo color
-        	mCitrusLogoColor =
-            (ColorPickerPreference) prefSet.findPreference(KEY_CITRUS_LOGO_COLOR);
-        	mCitrusLogoColor.setOnPreferenceChangeListener(this);
-            intColor = Settings.System.getInt(getContentResolver(),
-                Settings.System.STATUS_BAR_CITRUS_LOGO_COLOR, 0xffffffff);
-       		hexColor = String.format("#%08x", (0xffffffff & intColor));
-            mCitrusLogoColor.setSummary(hexColor);
-            mCitrusLogoColor.setNewPreviewColor(intColor);
+        // custom logo color
+        mCustomLogoColor =
+            (ColorPickerPreference) prefSet.findPreference(KEY_CUSTOM_LOGO_COLOR);
+        mCustomLogoColor.setOnPreferenceChangeListener(this);
+        int intColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.CUSTOM_LOGO_COLOR, 0xffffffff);
+        String hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mCustomLogoColor.setSummary(hexColor);
+            mCustomLogoColor.setNewPreviewColor(intColor);
 
-            mCitrusLogoStyle = (ListPreference) findPreference(KEY_CITRUS_LOGO_STYLE);
+
+            mCustomLogoStyle = (ListPreference) findPreference(KEY_CUSTOM_LOGO_STYLE);
             int citrusLogoStyle = Settings.System.getIntForUser(getContentResolver(),
-                    Settings.System.STATUS_BAR_CITRUS_LOGO_STYLE, 0,
+                    Settings.System.CUSTOM_LOGO_STYLE, 0,
                     UserHandle.USER_CURRENT);
-            mCitrusLogoStyle.setValue(String.valueOf(citrusLogoStyle));
-            mCitrusLogoStyle.setSummary(mCitrusLogoStyle.getEntry());
-            mCitrusLogoStyle.setOnPreferenceChangeListener(this);
+            mCustomLogoStyle.setValue(String.valueOf(citrusLogoStyle));
+            mCustomLogoStyle.setSummary(mCustomLogoStyle.getEntry());
+            mCustomLogoStyle.setOnPreferenceChangeListener(this);
+
     }
 
-	@Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mCitrusLogoColor) {
+        if (preference == mCustomLogoColor) {
             String hex = ColorPickerPreference.convertToARGB(
                     Integer.valueOf(String.valueOf(newValue)));
             preference.setSummary(hex);
             int intHex = ColorPickerPreference.convertToColorInt(hex);
             Settings.System.putInt(getContentResolver(),
-                    Settings.System.STATUS_BAR_CITRUS_LOGO_COLOR, intHex);
+                    Settings.System.CUSTOM_LOGO_COLOR, intHex);
             return true;
-        } else if (preference == mCitrusLogoStyle) {
+        }  else if (preference == mCustomLogoStyle) {
                 int citrusLogoStyle = Integer.valueOf((String) newValue);
-                int index = mCitrusLogoStyle.findIndexOfValue((String) newValue);
+                int index = mCustomLogoStyle.findIndexOfValue((String) newValue);
                 Settings.System.putIntForUser(
-                        getContentResolver(), Settings.System.STATUS_BAR_CITRUS_LOGO_STYLE, citrusLogoStyle,
+                       getContentResolver(), 
+		Settings.System.CUSTOM_LOGO_STYLE, citrusLogoStyle,
                         UserHandle.USER_CURRENT);
-                mCitrusLogoStyle.setSummary(
-                        mCitrusLogoStyle.getEntries()[index]);
+                mCustomLogoStyle.setSummary(
+                        mCustomLogoStyle.getEntries()[index]);
                 return true;
-        }
+	}
         return false;
     }
-
 
     @Override
     protected int getMetricsCategory() {

Cosmic-OS/platform_packages_apps_Galaxy

Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

src/com/cosmic/settings/fragments/CustomLogo.java
@@ -23,86 +23,82 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceScreen;
-import android.support.v7.preference.PreferenceCategory;
-import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceScreen;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 
 import com.android.settings.R;
-import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
-import com.android.internal.logging.MetricsProto.MetricsEvent;
-
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
-public class LogoSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+public class CustomLogo extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
 
-    public static final String TAG = "LogoSettings";
+    public static final String TAG = "Logo";
 
-    private static final String KEY_COSMIC_LOGO_COLOR = "status_bar_cosmic_logo_color";
-    private static final String KEY_COSMIC_LOGO_STYLE = "status_bar_cosmic_logo_style";
+    private static final String KEY_CUSTOM_LOGO_COLOR = "custom_logo_color";
+    private static final String KEY_CUSTOM_LOGO_STYLE = "custom_logo_style";
 
-    private ColorPickerPreference mCosmicLogoColor;
-    private ListPreference mCosmicLogoStyle;
+    private ColorPickerPreference mCustomLogoColor;
+    private ListPreference mCustomLogoStyle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.cosmic_logo);
+        addPreferencesFromResource(R.xml.custom_logo);
 
         PreferenceScreen prefSet = getPreferenceScreen();
 
-        int intColor;
-        String hexColor;
+        // custom logo color
+        mCustomLogoColor =
+            (ColorPickerPreference) prefSet.findPreference(KEY_CUSTOM_LOGO_COLOR);
+        mCustomLogoColor.setOnPreferenceChangeListener(this);
+        int intColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.CUSTOM_LOGO_COLOR, 0xffffffff);
+        String hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mCustomLogoColor.setSummary(hexColor);
+            mCustomLogoColor.setNewPreviewColor(intColor);
 
-        	// Cosmic-OS logo color
-        	mCosmicLogoColor =
-            (ColorPickerPreference) prefSet.findPreference(KEY_COSMIC_LOGO_COLOR);
-        	mCosmicLogoColor.setOnPreferenceChangeListener(this);
-            intColor = Settings.System.getInt(getContentResolver(),
-                Settings.System.STATUS_BAR_COSMIC_LOGO_COLOR, 0xffffffff);
-       		hexColor = String.format("#%08x", (0xffffffff & intColor));
-            mCosmicLogoColor.setSummary(hexColor);
-            mCosmicLogoColor.setNewPreviewColor(intColor);
 
-            mCosmicLogoStyle = (ListPreference) findPreference(KEY_COSMIC_LOGO_STYLE);
+            mCustomLogoStyle = (ListPreference) findPreference(KEY_CUSTOM_LOGO_STYLE);
             int cosmicLogoStyle = Settings.System.getIntForUser(getContentResolver(),
-                    Settings.System.STATUS_BAR_COSMIC_LOGO_STYLE, 0,
+                    Settings.System.CUSTOM_LOGO_STYLE, 0,
                     UserHandle.USER_CURRENT);
-            mCosmicLogoStyle.setValue(String.valueOf(cosmicLogoStyle));
-            mCosmicLogoStyle.setSummary(mCosmicLogoStyle.getEntry());
-            mCosmicLogoStyle.setOnPreferenceChangeListener(this);
+            mCustomLogoStyle.setValue(String.valueOf(cosmicLogoStyle));
+            mCustomLogoStyle.setSummary(mCustomLogoStyle.getEntry());
+            mCustomLogoStyle.setOnPreferenceChangeListener(this);
+
     }
 
-	@Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mCosmicLogoColor) {
+        if (preference == mCustomLogoColor) {
             String hex = ColorPickerPreference.convertToARGB(
                     Integer.valueOf(String.valueOf(newValue)));
             preference.setSummary(hex);
             int intHex = ColorPickerPreference.convertToColorInt(hex);
             Settings.System.putInt(getContentResolver(),
-                    Settings.System.STATUS_BAR_COSMIC_LOGO_COLOR, intHex);
+                    Settings.System.CUSTOM_LOGO_COLOR, intHex);
             return true;
-        } else if (preference == mCosmicLogoStyle) {
+        }  else if (preference == mCustomLogoStyle) {
                 int cosmicLogoStyle = Integer.valueOf((String) newValue);
-                int index = mCosmicLogoStyle.findIndexOfValue((String) newValue);
+                int index = mCustomLogoStyle.findIndexOfValue((String) newValue);
                 Settings.System.putIntForUser(
-                        getContentResolver(), Settings.System.STATUS_BAR_COSMIC_LOGO_STYLE, cosmicLogoStyle,
+                       getContentResolver(),
+		Settings.System.CUSTOM_LOGO_STYLE, cosmicLogoStyle,
                         UserHandle.USER_CURRENT);
-                mCosmicLogoStyle.setSummary(
-                        mCosmicLogoStyle.getEntries()[index]);
+                mCustomLogoStyle.setSummary(
+                        mCustomLogoStyle.getEntries()[index]);
                 return true;
-        }
+	}
         return false;
     }
-
 
     @Override
     protected int getMetricsCategory() {