31afaef15be9a29f5fcaa11274999131ce818d0d
Shubham Singh <coolsks94@gmail.com>
2017-05-09 11:27:55 +0530

    Systemappremover:Fix it for priv-apps It was removing apps from wrong path
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- src/com/cosmic/settings/fragments/SystemappRemover.java
+++ src/com/cosmic/settings/fragments/SystemappRemover.java
@@ -449,7 +449,7 @@ private String[] combine(String[] a, String[] b) {
             for (String appName : params) {
       		String odexAppName = appName.replaceAll(".apk$", ".odex");
                 String basePath = systemPath;
-                 File app = new File(systemPath);
+                 File app = new File(systemPath + appName);
 
                 if( ! app.exists() )
                     basePath = systemPrivPath;
