33a967ecfeaf7aa12ccd1102858ac32deef0cbd8
Adin Kwok <adin.kwok@carbonrom.org>
2018-08-11 02:40:33 -0700

    Galaxy: Smart Pixels [2/2]
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

---
+++ src/com/cosmic/settings/display/SmartPixels.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2018 Cosmic-OS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cosmic.settings.display;
+
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.cosmic.CustomSettingsPreferenceFragment;
+
+public class SmartPixels extends CustomSettingsPreferenceFragment {
+    private static final String TAG = "SmartPixels";
+    private static final String SMART_PIXELS_ENABLE = "smart_pixels_enable";
+    private static final String SMART_PIXELS_ON_POWER_SAVE = "smart_pixels_on_power_save";
+
+    private SmartPixelsObserver mSmartPixelsObserver;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.smart_pixels);
+
+        addCustomPreference(findPreference(SMART_PIXELS_ENABLE), SYSTEM_TWO_STATE, STATE_OFF);
+        addCustomPreference(findPreference(SMART_PIXELS_ON_POWER_SAVE), SYSTEM_TWO_STATE, STATE_OFF);
+        mSmartPixelsObserver = new SmartPixelsObserver(new Handler());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mSmartPixelsObserver != null) {
+            mSmartPixelsObserver.register();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mSmartPixelsObserver != null) {
+            mSmartPixelsObserver.unregister();
+        }
+    }
+
+    private class SmartPixelsObserver extends ContentObserver {
+        public SmartPixelsObserver(Handler handler) {
+            super(handler);
+        }
+
+        public void register() {
+            getActivity().getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    SMART_PIXELS_ENABLE), false, this, UserHandle.USER_CURRENT);
+            getActivity().getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    SMART_PIXELS_ON_POWER_SAVE), false, this, UserHandle.USER_CURRENT);
+        }
+
+        public void unregister() {
+            getActivity().getContentResolver().unregisterContentObserver(this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            super.onChange(selfChange);
+            updateAllCustomPreferences();
+        }
+    }
+}
--- src/com/cosmic/settings/fragments/DisplaySettings.java
+++ src/com/cosmic/settings/fragments/DisplaySettings.java
@@ -8,11 +8,23 @@ import com.android.settings.SettingsPreferenceFragment;
 
 public class MiscSettings extends CustomSettingsPreferenceFragment {
 
+    private static final String SMART_PIXELS = "smart_pixels";
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.display_settings);
+        updateSmartPixelsPreference();
+    }
 
+    private void updateSmartPixelsPreference() {
+        PreferenceScreen prefSet = getPreferenceScreen();
+        boolean enableSmartPixels = getContext().getResources().
+                getBoolean(com.android.internal.R.bool.config_enableSmartPixels);
+        Preference smartPixels = findPreference(SMART_PIXELS);
+         if (!enableSmartPixels){
+            prefSet.removePreference(smartPixels);
+        }
     }
 }
