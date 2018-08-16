bf78d5c6a501ff1591e4fb6e3bbd6534df769589
maxwen <max.weninger@gmail.com>
2018-08-15 17:51:49 -0700

    Galaxy: Global Actions with Advanced Reboot [2/2]
    Cosmic edits:
    * Adapted for our new CustomSettingsPreferenceManager
    
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- src/com/cosmic/settings/fragments/MiscSettings.java
+++ src/com/cosmic/settings/fragments/MiscSettings.java
@@ -8,11 +8,13 @@ import com.android.settings.SettingsPreferenceFragment;
 
 public class MiscSettings extends CustomSettingsPreferenceFragment {
 
+    private static final String ADVANCED_REBOOT = "advanced_reboot";
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.misc_settings);
-
+        addCustomPreference(findPreference(ADVANCED_REBOOT), SECURE_TWO_STATE, STATE_ON);
     }
 }
