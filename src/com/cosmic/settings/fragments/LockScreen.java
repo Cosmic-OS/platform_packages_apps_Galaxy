00fd2152970f4ed16b9262f6f242bec0827fe2d3
Adnan <adnan@cyngn.com>
2018-08-16 18:54:03 -0700

    Galaxy: Add option to scramble pin layout when unlocking [2/2]
    Change-Id: I3e2c200a0a31d3c765831bc30280029a50c88051
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>

--- src/com/cosmic/settings/fragments/LockScreen.java
+++ src/com/cosmic/settings/fragments/LockScreen.java
@@ -7,11 +7,13 @@ import com.android.settings.cosmic.CustomSettingsPreferenceFragment;
 
 public class LockScreen extends CustomSettingsPreferenceFragment {
     private static final String TAG = "LockScreen";
+    private static final String LOCKSCREEN_PIN_SCRAMBLE_LAYOUT = "lockscreen_scramble_pin_layout";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.lock_screen);
+        addCustomPreference(findPreference(LOCKSCREEN_PIN_SCRAMBLE_LAYOUT), SYSTEM_TWO_STATE, STATE_OFF);
     }
 }
