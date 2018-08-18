5ff36d2fb2f443674fad386f89a813c9cf74fefc
maxwen <max.weninger@gmail.com>
2018-08-17 17:20:20 -0700

    Galaxy: volume key music control [2/2]
    Change-Id: Iab7967fcd1efbe353ea74e41f40e40e17ad8c6b6

--- src/com/cosmic/settings/fragments/Buttons.java
+++ src/com/cosmic/settings/fragments/Buttons.java
@@ -7,11 +7,13 @@ import com.android.settings.cosmic.CustomSettingsPreferenceFragment;
 
 public class Buttons extends CustomSettingsPreferenceFragment {
     private static final String TAG = "Buttons";
+    private static final String VOLUME_BUTTON_MUSIC_CONTROL = "volume_button_music_control";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        addCustomPreference(findPreference(VOLUME_BUTTON_MUSIC_CONTROL), SYSTEM_TWO_STATE, STATE_OFF);
         addPreferencesFromResource(R.xml.buttons);
     }
 }
