e4c799fe79b9bc7b80f703cf52053f50b5c224d3
Matt Mower <mowerm@gmail.com>
2018-08-18 00:00:11 -0700

    Galaxy: Forward port lockscreen quick unlock [2/2]
    Adapted to Galaxy
    
    Change-Id: I3a08ba32918dada705ffb9ff13247cc98c844feb
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- src/com/cosmic/settings/fragments/LockScreen.java
+++ src/com/cosmic/settings/fragments/LockScreen.java
@@ -7,6 +7,8 @@ import com.android.settings.cosmic.CustomSettingsPreferenceFragment;
 
 public class LockScreen extends CustomSettingsPreferenceFragment {
     private static final String TAG = "LockScreen";
+
+    private static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "lockscreen_quick_unlock_control";
     private static final String LOCKSCREEN_PIN_SCRAMBLE_LAYOUT = "lockscreen_scramble_pin_layout";
     private static final String STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD = "status_bar_locked_on_secure_keyguard";
 
@@ -15,6 +17,7 @@ public class LockScreen extends CustomSettingsPreferenceFragment {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.lock_screen);
+        addCustomPreference(findPreference(LOCKSCREEN_QUICK_UNLOCK_CONTROL), SYSTEM_TWO_STATE, STATE_OFF);
         addCustomPreference(findPreference(LOCKSCREEN_PIN_SCRAMBLE_LAYOUT), SYSTEM_TWO_STATE, STATE_OFF);
         addCustomPreference(findPreference(STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD), SECURE_TWO_STATE, STATE_OFF);
     }
