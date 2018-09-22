24597da709be42c6be3c2ea6e98d2ad232f9a819
Sagar Shah <sagarshah2326@gmail.com>
2018-09-22 16:11:43 +0000

    Galaxy: Add Display category
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

---
+++ src/com/cosmic/settings/fragments/DisplaySettings.java
@@ -0,0 +1,18 @@
+package com.cosmic.settings.fragments;
+
+import android.os.Bundle;
+import com.android.settings.R;
+import com.android.settings.cosmic.CustomSettingsPreferenceFragment;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+public class MiscSettings extends CustomSettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        addPreferencesFromResource(R.xml.display_settings);
+
+    }
+}
