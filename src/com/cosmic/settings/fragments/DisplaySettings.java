696cdf3b64acc4d7564a48d97a379ce4f93e61cd
Sagar Shah <sagarshah2326@gmail.com>
2018-09-24 00:23:01 +0530

    Galaxy: Add Preference and PreferenceScreen imports
    * Missing in DisplaySettings.java

--- src/com/cosmic/settings/fragments/DisplaySettings.java
+++ src/com/cosmic/settings/fragments/DisplaySettings.java
@@ -1,6 +1,9 @@
 package com.cosmic.settings.fragments;
 
 import android.os.Bundle;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+
 import com.android.settings.R;
 import com.android.settings.cosmic.CustomSettingsPreferenceFragment;
 
