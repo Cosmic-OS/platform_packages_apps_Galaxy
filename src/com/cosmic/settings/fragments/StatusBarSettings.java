1f87c71b2b9bd42eae0b8773004c5819f59b7e6d
LorDClockaN <lordclockan@gmail.com>
2019-06-10 11:08:56 +0700

    Cosmic-OS logo in the status bar [2/2]
    Change-Id: I899f7c1e38cba7f840e98f89b31807c56cf5abf2
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- src/com/cosmic/settings/fragments/StatusBarSettings.java
+++ src/com/cosmic/settings/fragments/StatusBarSettings.java
@@ -18,13 +18,14 @@ package com.cosmic.settings.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
 
-import android.os.Bundle;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.os.UserHandle;
-import android.content.ContentResolver;
 import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceGroup;
@@ -33,27 +34,32 @@ import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.PreferenceFragment;
 import android.support.v14.preference.SwitchPreference;
-import android.provider.Settings;
-import com.android.settings.R;
-
-import java.util.Locale;
 import android.text.TextUtils;
 import android.view.View;
 
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
+
 import android.util.Log;
 
-import java.util.List;
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.List;
 
 public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String KEY_HIDE_NOTCH = "statusbar_hide_notch";
+    private static final String KEY_STATUS_BAR_LOGO = "status_bar_logo";
+
+    private SwitchPreference mShowCosmicLogo;
+    private ListPreference mLogoStyle;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -72,11 +78,36 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
             getPreferenceScreen().removePreference(hideNotchPref);
         }
 
+        mShowCosmicLogo = (SwitchPreference) findPreference(KEY_STATUS_BAR_LOGO);
+        mShowCosmicLogo.setChecked((Settings.System.getInt(getContentResolver(),
+             Settings.System.STATUS_BAR_LOGO, 0) == 1));
+        mShowCosmicLogo.setOnPreferenceChangeListener(this);
+
+        mLogoStyle = (ListPreference) findPreference("status_bar_logo_style");
+        mLogoStyle.setOnPreferenceChangeListener(this);
+        int logoStyle = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.STATUS_BAR_LOGO_STYLE,
+                0, UserHandle.USER_CURRENT);
+        mLogoStyle.setValue(String.valueOf(logoStyle));
+        mLogoStyle.setSummary(mLogoStyle.getEntry());
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
+        if  (preference == mShowCosmicLogo) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUS_BAR_LOGO, value ? 1 : 0);
+            return true;
+        } else if (preference.equals(mLogoStyle)) {
+            int logoStyle = Integer.parseInt(((String) objValue).toString());
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.STATUS_BAR_LOGO_STYLE, logoStyle, UserHandle.USER_CURRENT);
+            int index = mLogoStyle.findIndexOfValue((String) objValue);
+            mLogoStyle.setSummary(
+                    mLogoStyle.getEntries()[index]);
+            return true;
+        }
         return false;
     }
 
